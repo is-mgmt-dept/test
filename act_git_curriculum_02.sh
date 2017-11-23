f82bb281047b09759436ecbae5d5291078a3aa35
Kodai Kawase <>
2017-11-24 02:04:03 +0900

    gitカリキュラム用シェルスクリプト作成

---
+++ act_git_curriculum_02.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# gitカリキュラムクローン用ディレクトリ作成
+mkdir /c/pull_test
+# 一時的に移動
+cd /c/pull_test
+# gitカリキュラムをクローン
+git clone https://github.com/is-mgmt-dept/test.git
+# クローンしたリポジトリに移動
+cd /c/pull_test/test
+# ダミーのユーザ名とユーザメールアドレスを設定
+git config user.name "is-mgmt-dept"
+git config user.email ""
+# 更新が発生するようにREADME.mdにdateコマンドの結果を追加
+echo -e "\n 更新日は`date`\n" >> README.md
+echo -e "\n私の名前は`whoami`\n" >> README.md
+# リモートリポジトリへプッシュ
+git add README.md
+git commit -m "gitカリキュラム pull用コミット"
+git push origin master
+# ディレクトリを削除
+rm -rf /c/pull_test
